f988ca37c555f04cd942ee3f497998cc4e9d9a81
Anh Nguyen <anhng22@gmail.com>
2014-03-05 11:09:03 +0700

    chapter 14

--- app/controllers/subjects_controller.rb
+++ app/controllers/subjects_controller.rb
@@ -1,7 +1,9 @@
 class SubjectsController < ApplicationController
-  
+
   layout "admin"
-  
+
+  before_action :confirm_logged_in
+
   def index
     @subjects = Subject.sorted
   end
@@ -13,77 +15,61 @@ class SubjectsController < ApplicationController
   def new
     @subject = Subject.new({:name => "Default"})
     @subject_count = Subject.count + 1
-    
   end
-  
+
   def create
     # Instantiate a new object using form parameters
     @subject = Subject.new(subject_params)
-    
     # Save the object
     if @subject.save
-      
-      flash[:notice] = "Subject created successfully."
-      
       # If save succeeds, redirect to the index action
+      flash[:notice] = "Subject created successfully."
       redirect_to(:action => 'index')
-      
-      # If save fails, redispaly the form so the user can fix problems
     else
+      # If save fails, redisplay the form so user can fix problems
       @subject_count = Subject.count + 1
       render('new')
     end
-    
-      
   end
 
   def edit
     @subject = Subject.find(params[:id])
     @subject_count = Subject.count
   end
-  
+
   def update
-     # Find an existing object using form parameters
-     @subject = Subject.find(params[:id])
-     
-     # Update the object
-     if @subject.update_attributes(subject_params)
-       
-       flash[:notice] = "Subject updated successfully."
-
-       # If save succeeds, redirect to the index action
-       redirect_to(:action => 'show', :id => @subject.id)
-
-       # If save fails, redispaly the form so the user can fix problems
-     else
-       @subject_count = Subject.count
-       render('edit')
-     end
-
-   end
+    # Find an existing object using form parameters
+    @subject = Subject.find(params[:id])
+    # Update the object
+    if @subject.update_attributes(subject_params)
+      # If update succeeds, redirect to the index action
+      flash[:notice] = "Subject updated successfully."
+      redirect_to(:action => 'show', :id => @subject.id)
+    else
+      # If update fails, redisplay the form so user can fix problems
+      @subject_count = Subject.count
+      render('edit')
+    end
+  end
 
   def delete
     @subject = Subject.find(params[:id])
   end
-  
+
   def destroy
     subject = Subject.find(params[:id]).destroy
-    
     flash[:notice] = "Subject '#{subject.name}' destroyed successfully."
-    
     redirect_to(:action => 'index')
   end
-  
+
+
   private
 
-     def subject_params
-       # same as using "params[:subject]", except that it:
-       # - raises an error if :subject is not present
-       # - allows listed attributes to be mass-assigned
-       params.require(:subject).permit(:name, :position, :visible)
-     end
+    def subject_params
+      # same as using "params[:subject]", except that it:
+      # - raises an error if :subject is not present
+      # - allows listed attributes to be mass-assigned
+      params.require(:subject).permit(:name, :position, :visible, :created_at)
+    end
 
-  
 end
-
-  
