828ead3e70d63f5515edde8c7af97e8401332aec
Anh Nguyen <anhng22@gmail.com>
2014-03-07 09:26:44 +0700

    chapter 15

--- app/models/admin_user.rb
+++ app/models/admin_user.rb
@@ -8,6 +8,9 @@ class AdminUser < ActiveRecord::Base
   has_and_belongs_to_many :pages
   has_many :section_edits
   has_many :sections, :through => :section_edits
+  
+
+
 
   EMAIL_REGEX = /\A[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}\Z/i
   FORBIDDEN_USERNAMES = ['littlebopeep','humptydumpty','marymary']
@@ -38,6 +41,16 @@ class AdminUser < ActiveRecord::Base
 
   validate :username_is_allowed
   #validate :no_new_users_on_saturday, :on => :create
+  
+  scope :sorted, lambda { order("last_name ASC, first_name ASC")}  
+  
+  def name
+    "#{first_name} #{last_name}"
+    # or: first_name + ' ' + last_name
+    # or: [first_name, last_name].join(' ')
+  end
+  
+  private
 
   def username_is_allowed
     if FORBIDDEN_USERNAMES.include?(username)
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 SimpleCms::Application.routes.draw do
   
+  #get "admin_users/index"
+  #get "admin_users/new"
+  #get "admin_users/edit"
+  #get "admin_users/delete"
   root "demo#index"
   get 'admin', :to => "access#index"  
   match ':controller(/:action(/:id))', :via => [:get, :post]
