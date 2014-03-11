2538f64168c9298de523b345e46ef656cb607c7f
Anh Nguyen <anhng22@gmail.com>
2014-03-12 05:35:23 +0700

    final

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,11 @@
 SimpleCms::Application.routes.draw do
-  
+
   #get "admin_users/index"
   #get "admin_users/new"
   #get "admin_users/edit"
   #get "admin_users/delete"
-  root "demo#index"
+  root "public#index"
+  get 'show/:permalink', :to => 'public#show'
   get 'admin', :to => "access#index"  
   match ':controller(/:action(/:id))', :via => [:get, :post]
   
